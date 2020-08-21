3faaddcf7e91be2ead9ebcc3fdf9164231c957d8
WilsonKinyua <wilsonkinyuam@gmail.com>
2020-08-21 03:20:38 +0300

    working to auth

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB5XxxynEFrEzkQT9t5V_j7USGnHhWUvHU",
+    authDomain: "oshop-store-b484f.firebaseapp.com",
+    databaseURL: "https://oshop-store-b484f.firebaseio.com",
+    projectId: "oshop-store-b484f",
+    storageBucket: "oshop-store-b484f.appspot.com",
+    messagingSenderId: "1069180797676",
+    appId: "1:1069180797676:web:d1350a87686d02b6ee5bc9",
+    measurementId: "G-XB0XB146ZH"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB5XxxynEFrEzkQT9t5V_j7USGnHhWUvHU",
+    authDomain: "oshop-store-b484f.firebaseapp.com",
+    databaseURL: "https://oshop-store-b484f.firebaseio.com",
+    projectId: "oshop-store-b484f",
+    storageBucket: "oshop-store-b484f.appspot.com",
+    messagingSenderId: "1069180797676",
+    appId: "1:1069180797676:web:d1350a87686d02b6ee5bc9",
+    measurementId: "G-XB0XB146ZH"
+  }
 };
 
 /*
